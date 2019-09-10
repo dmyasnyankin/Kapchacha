f98a47cbe500cf90f15e8f3f8b7da26b2e56440f
Dennis Myasnyankin <dennis415@live.com>
2019-09-10 15:56:43 -0700

    Create method to start the captcha request and parse thru the result

--- captcha.js
+++ captcha.js
@@ -10,25 +10,36 @@ const chromeOptions = {
     const page = await browser.newPage();
     await page.goto('https://old.reddit.com/login');
 
+    const requestId = initiateCaptchaRequest(apiKey);
+
     await page.type('#user_reg', 'itsnumbawanna');
     await page.type('#passwd_reg', 'holymolyDonut$hop');
     await page.type('#passwd2_reg', 'holymolyDonut$hop');
     await page.type('#email_reg', 'yaboy@gmail.com');
+
+    const response = await pollForRequestResults(apiKey, requestId);
+
+    await page.evaluate(`document.getElementById("g-recaptcha-response").innerHTML="${response}";`);
+
     await page.click('#register-form button');
 })()
 
 // 2Captcha related
+async function initiateCaptchaRequest(apiKey){
+    const formData = {
+        method: 'userrecaptcha',
+        key: apiKey, // my apikey for 2cap
+        googlekey: '6LeTnxkTAAAAAN9QEuDZRpn90WwKk_R1TRW_g-JC', 
+        // ^^^gotten from elements on page ur scraping under data site key
+        pageurl: 'https://old.reddit.com/login',
+        // url I am scraping
+        json: 1
+    };
+    
+    const response = await request.post('http://2captcha.com/in.php', {form: formData});
+    
+    const requestId = JSON.parse(response).request;
 
-const formData = {
-    method: 'usershmoozer',
-    key: apiKey, // my apikey for 2cap
-    googlekey: '6LeTnxkTAAAAAN9QEuDZRpn90WwKk_R1TRW_g-JC', 
-    // ^^^gotten from elements on page ur scraping under data site key
-    pageurl: 'https://old.reddit.com/login',
-    // url I am scraping
-    json: 1
-};
+    return requestId;
+}
 
-const response = await request.post('http://2captcha.com/in.php', {form: formData});
-
-const requestId = JSON.parse(response).request;
